2446466702f75cf2be0565dbb154da83e4812b89
e <admin@mlit.me>
2019-05-30 09:23:18 +0800

    优化检测逻辑 提升页面加载速度

--- admin/js/main.js
+++ admin/js/main.js
@@ -17,10 +17,9 @@ layui.use(['form', 'element', 'layer', 'jquery'], function () {
         type: "get",
         dataType: "json",
         success: function (data) {
-            var item = data.data,
-                this_version = item.version;
+            var item = data.data;
             user = item.user;
-            $(".version").text(this_version);// 当前版本
+            $(".version").text(item.version);// 当前版本
             $(".php_version").text(item.php_version);// PHP版本
             $(".server").text(item.server);// 服务器环境
             $(".host").text(item.host);// 服务器 (IP/端口)
@@ -28,17 +27,18 @@ layui.use(['form', 'element', 'layer', 'jquery'], function () {
             $(".upload_max").text(item.upload_max);// 允许最大上传文件
             $(".root").text(item.root);// 安装目录
             $(".loginTime").text(item.loginTime);// 上次登录时间
-            // 检测更新
-            $.get('https://pan.amoli.co/AmoliCloud.php?act=version', function (data) {
-                var new_version = data.version;
-                if (new_version != this_version) {
-                    $("#new_version,#new").css('color', '#FF5722');
-                    $("#new").text('有新版本可用');
-                    $("#new_version").text('点击查看更新内容');
-                } else {
-                    $("#new_version").text(new_version);
-                }
-            })
+        }
+    })
+
+    // 检测更新
+    $.get('https://pan.amoli.co/AmoliCloud.php?act=version', function (data) {
+        var new_version = data.version;
+        if (new_version != $(".version").text()) {
+            $("#new_version,#new").css('color', '#FF5722');
+            $("#new").text('更新提示');
+            $("#new_version").text('有新版本可以更新');
+        } else {
+            $("#new_version").text(new_version);
         }
     })
 })
