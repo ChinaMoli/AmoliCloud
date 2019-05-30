8997f5ef8fb9e45ed2797e3ef05afb56a9aa0098
e <admin@mlit.me>
2019-05-30 08:52:51 +0800

    删除多余文件夹整合文件

--- admin/js/index.js
+++ admin/js/index.js
@@ -11,7 +11,7 @@ layui.use(['bodyTab', 'form', 'element', 'layer', 'jquery'], function () {
 		layer = parent.layer === undefined ? layui.layer : top.layer;
 	tab = layui.bodyTab({
 		openTabNum: "50",  //最大可打开窗口数量
-		url: "json/navs.json" //获取菜单json地址
+		url: "js/navs.json" //获取菜单json地址
 	});
 
 	// 判断是否登录
